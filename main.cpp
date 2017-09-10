6338f9fbfdad222cbfa436e77f461af17efecd60
daqi <daqi@xiaomi.com>
2017-09-10 18:58:57 +0800

    cmake variable replacement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "foo.h"
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    cout << "VAR: " << MY_VAR <<endl;
+
     return 0;
 }
