d124d2ecfbdbac019e1d27d96c7fa439656801dc
daqi <daqi@xiaomi.com>
2017-09-24 13:26:35 +0800

    fork

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
 
-void my_exit() {
-    printf("my_exit\n");
-}
 
 int main() {
-    atexit(my_exit);
-    atexit(my_exit);
-    exit(1);
+//    printf("pid: %d\n", getpid());
+//    printf("ppid: %d\n", getppid());
+//    printf("uid: %d\n", getuid());
+
+    int pid;
+    if ((pid = fork()) < 0) {
+        printf("fork fail\n");
+    } else if (pid == 0) {
+        printf("Child process: %d\n", getpid());
+    } else {
+        printf("Parent process: %d\n", getpid());
+    }
+
     return 0;
 }
