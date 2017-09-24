6ce0837e5e8a883926f0e14622337e50dca45a57
daqi <daqi@xiaomi.com>
2017-09-24 14:53:02 +0800

    sigaction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+void sig_handler(int sig) {
+    printf("get sig %d\n", sig);
+    exit(100);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printf("sigchld %d\n", SIGCHLD);
+    printf("pid: %d\n", getpid());
+
+//    struct sigaction sig = {
+//            .sa_handler = sig_handler,
+//            .sa_flags = 0,
+//    };
+    struct sigaction sig;
+    sig.sa_handler = sig_handler;
+    sig.sa_flags = 0;
+    sigemptyset(&sig.sa_mask);
+
+    struct sigaction oact;
+    printf("sig result: %d\n", sigaction(3, &sig, &oact));
+
+    while(1) {
+        printf("loop\n");
+        sleep(5);
+    }
+
     return 0;
 }
