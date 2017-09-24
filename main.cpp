6f83b4c0b0e7150a896690b6c1cac8c9ead344b0
daqi <daqi@xiaomi.com>
2017-09-24 10:41:39 +0800

    open/write a file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int fd = open("foo.txt", O_RDWR|O_CREAT, S_IWUSR|S_IRUSR);
+    printf("fd: %d\n", fd);
+    char* buf = "Hello C";
+    int sz = write(fd, buf, 7);
+    printf("write res: %d", sz);
+    close(fd);
+
     return 0;
 }
