25c881102b8224d581e26b925b5eb6d4e6381759
daqi <daqi@xiaomi.com>
2017-09-24 13:07:00 +0800

    atexit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
+
+void my_exit() {
+    printf("my_exit\n");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    atexit(my_exit);
+    atexit(my_exit);
+    exit(1);
     return 0;
 }
