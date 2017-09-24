54c69789406495793e7553d202471078ab210938
daqi <daqi@xiaomi.com>
2017-09-24 14:12:28 +0800

    execl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 int main() {
@@ -13,8 +14,14 @@ int main() {
         printf("fork fail\n");
     } else if (pid == 0) {
         printf("Child process: %d\n", getpid());
+        printf("exec returns: %d\n", execlp("cp", ""));
     } else {
         printf("Parent process: %d\n", getpid());
+        int status;
+        while(wait(&status) < 0) {
+            printf("wait fail\n");
+        }
+        printf("child died\n");
     }
 
     return 0;
