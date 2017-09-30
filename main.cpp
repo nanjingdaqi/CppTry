22c5bbe32143c6c7298e4b8262a6822cb13416c9
daqi <daqi@xiaomi.com>
2017-09-30 14:00:43 +0800

    add quit function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string.h>
+
+void quit(char* msg, int exit_code) {
+    size_t len = strlen(msg);
+    char suffix[] = ": %d";
+    char* buffer = static_cast<char *>(malloc(len + strlen(suffix)));
+    strcpy(buffer, msg);
+    strcat(buffer, suffix);
+    printf(buffer, errno);
+    exit(exit_code);
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
