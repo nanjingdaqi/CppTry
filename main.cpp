32b3f2f060f51c6a4eb05b4bf32a8813c1006070
daqi <daqi@xiaomi.com>
2017-10-11 09:18:38 +0800

    thread select

--- main.cpp
+++ main.cpp
@@ -12,42 +12,51 @@ void quit(char* msg, int exit_code) {
     exit(exit_code);
 }
 
+void* thread_read(void* args) {
+    int* fds = (int*) args;
+    int nfds = fds[0] + 1;
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(fds[0], &readfds);
+    printf("begin selecting\n");
+    int s = select(nfds, &readfds, NULL, NULL, NULL);
+    if (s == -1) {
+        quit("select readfds fail", -1);
+    }
+    printf("select returns\n");
+    char buf[20];
+    s = read(fds[0], buf, 1);
+    if (s == -1) {
+        quit("read fds[0] fail\n", -1);
+    }
+    printf("get msg: %s\n", buf);
+}
+
+void* thread_write(void* args) {
+    int* fds = (int*) args;
+    sleep(3);
+    int s = write(fds[1], "H", 1);
+    if (s < 0) {
+        quit("write to fds[1] fail\n", -1);
+    }
+}
+
 int main() {
     int fds[2];
     if (pipe(fds) == -1) {
         quit("pipe create error", -1);
     }
-    int pid = fork();
-    if (pid == 0) {
-        // child
-        if (close(fds[1]) == -1) {
-            quit("child close write fail", -1);
-        }
-        int nfds = fds[0] + 1;
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(fds[0], &readfds);
-        int ret = select(nfds, &readfds, NULL, NULL, NULL);
-        if (ret == -1) {
-            quit("child select fail", -1);
-        }
-        printf("child select returns: %d\n", ret);
-        char buf[10];
-        if (!FD_ISSET(fds[0], &readfds)) {
-            quit("fds[0] is not in readfds", -1);
-        }
-        if (read(fds[0], buf, 1) == -1) {
-            quit("read fds[0] fail", -1);
-        }
-        printf(buf);
-    } else {
-        // parent
-        if (close(fds[0] == -1)) {
-            quit("parent close read fail", -1);
-        }
-        sleep(3);
-        if (write(fds[1], "H", 1) == -1) {
-            quit("parent write fail", -1);
-        }
+
+    pthread_t t_r, t_w;
+    int s = pthread_create(&t_r, NULL, thread_read, fds);
+    if (s != 0) {
+        quit("pthread_create fail", -1);
     }
+    s = pthread_create(&t_w, NULL, thread_write, fds);
+    if (s != 0) {
+        quit("pthread_create fail", -1);
+    }
+    pthread_join(t_r, NULL);
+    pthread_join(t_w, NULL);
+    return 0;
 }
