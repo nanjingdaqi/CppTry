38306c9dfd61b5d15ab837099b8702f581e2977c
daqi <daqi@xiaomi.com>
2017-10-11 09:42:02 +0800

    thread epoll

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
+#include <sys/epoll.h>
+
+struct ep_arg {
+    int epfd;
+    int* fds;
+};
 
 void quit(char* msg, int exit_code) {
     size_t len = strlen(msg);
@@ -13,18 +19,26 @@ void quit(char* msg, int exit_code) {
 }
 
 void* thread_read(void* args) {
-    int* fds = (int*) args;
-    int nfds = fds[0] + 1;
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(fds[0], &readfds);
-    printf("begin selecting\n");
-    int s = select(nfds, &readfds, NULL, NULL, NULL);
+    ep_arg* ep_a = (ep_arg*) args;
+    int* fds = ep_a->fds;
+    int epfd = ep_a->epfd;
+
+    epoll_event ev;
+    ev.data.fd = fds[0];
+    ev.events = EPOLLIN;
+    int s = epoll_ctl(epfd, EPOLL_CTL_ADD, fds[0], &ev);
     if (s == -1) {
-        quit("select readfds fail", -1);
+        quit("epoll add event fail", -1);
     }
-    printf("select returns\n");
-    char buf[20];
+
+    printf("start epoll waiting\n");
+    epoll_event ev_list[5];
+    s = epoll_wait(epfd, ev_list, 5, -1);
+    if (s == -1) {
+        quit("epoll wait fail", -1);
+    }
+
+    char buf[1];
     s = read(fds[0], buf, 1);
     if (s == -1) {
         quit("read fds[0] fail\n", -1);
@@ -33,7 +47,8 @@ void* thread_read(void* args) {
 }
 
 void* thread_write(void* args) {
-    int* fds = (int*) args;
+    ep_arg* ep_a = (ep_arg*) args;
+    int* fds = ep_a->fds;
     sleep(3);
     int s = write(fds[1], "H", 1);
     if (s < 0) {
@@ -46,13 +61,20 @@ int main() {
     if (pipe(fds) == -1) {
         quit("pipe create error", -1);
     }
+    int epfd = epoll_create(5);
+    if (epfd == -1) {
+        quit("epoll_create fail", -1);
+    }
+    ep_arg arg;
+    arg.epfd = epfd;
+    arg.fds = fds;
 
     pthread_t t_r, t_w;
-    int s = pthread_create(&t_r, NULL, thread_read, fds);
+    int s = pthread_create(&t_r, NULL, thread_read, &arg);
     if (s != 0) {
         quit("pthread_create fail", -1);
     }
-    s = pthread_create(&t_w, NULL, thread_write, fds);
+    s = pthread_create(&t_w, NULL, thread_write, &arg);
     if (s != 0) {
         quit("pthread_create fail", -1);
     }
